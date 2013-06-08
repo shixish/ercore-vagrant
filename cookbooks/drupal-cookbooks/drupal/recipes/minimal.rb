4062573f3d349bcc6427ec201a1ddaa189393afb
Andrew <shixish@gmail.com>
2013-06-07 14:03:52 -1000

    changing to ubuntu 12.0

--- cookbooks/drupal-cookbooks/drupal/recipes/minimal.rb
+++ cookbooks/drupal-cookbooks/drupal/recipes/minimal.rb
@@ -18,17 +18,6 @@ pkgs.each do |pkg|
   end
 end
 
-php_pear "pdo" do
-  action :install
-end
-
-# Install APC for increased performance. rfc1867 support also provides minimal
-# feedback for file uploads.  Requires pcre library.
-php_pear "apc" do
-  directives(:shm_size => "70M", :rfc1867 => 1, :include_once_override => 0)
-  version "3.1.6" # TODO Somehow Chef PEAR/PECL provider causes debugging to be enabled on later builds.
-  action :install
-end
 
 # Install uploadprogress for better feedback during Drupal file uploads.
 php_pear "uploadprogress" do
